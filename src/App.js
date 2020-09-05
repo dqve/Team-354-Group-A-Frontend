b25e4681790c738d57c9cc65c9931944069523da
haffs <abuafeezabolarinwa@yahoo.com>
2020-09-05 06:36:45 +0100

    update app component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import HomePage from './pages/homepage/HomePage';
-import PageNotFound from './pages/pagenotfound/PageNotFound';
-
-
+import React from "react"
+import { Route, Switch } from "react-router-dom"
+import { ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
+import HomePage from "./pages/homepage/HomePage"
+import PageNotFound from "./pages/pagenotfound/PageNotFound"
 
 const App = () => (
-    <div className="container-fluid">
-        <Switch>
-            <Route exact path="/" component={HomePage}/>
-            <Route component={PageNotFound}/>
-        </Switch>
-        <ToastContainer autoClose={3000} hideProgressBar />
-    </div>
+  <div className="container-fluid">
+    <Switch>
+      <Route exact path="/" component={HomePage} />
+      <Route component={PageNotFound} />
+    </Switch>
+    <ToastContainer autoClose={3000} hideProgressBar />
+  </div>
 )
 
-export default App;
+export default App
