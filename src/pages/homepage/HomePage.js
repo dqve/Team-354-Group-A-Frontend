5edc9f968b85d3a8713204f436b046f72a3ea44f
dqve <ayooladavid@gmail.com>
2020-09-07 10:23:56 +0100

    Delete unused dependencies and update Nav Links

--- src/pages/homepage/HomePage.js
+++ src/pages/homepage/HomePage.js
@@ -1,8 +1,9 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 
 const HomePage = () => (<div class="alert alert-primary" role="alert">
                             Welcome home!!!!!!!
+                            <Link to="/landing" className="success">Visit Landing Page</Link>
                         </div>)
 
 export default HomePage;
